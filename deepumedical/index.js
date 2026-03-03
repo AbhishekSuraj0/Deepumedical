a3ec609ebd10469509347221a29e93972b0c2fc1
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-03-03 10:18:40 +0530

    dd

--- deepumedical/index.js
+++ deepumedical/index.js
@@ -50,6 +50,7 @@ inputsearch.addEventListener("input", () => {
     dataresult.innerHTML = "this medicine is not available";
     return;
   }
+  document.getElementById('totalmedicine').textContent = `Total medicine : ${result.length}`
 
   result.sort().forEach((item) => {
     const div = document.createElement("div");
