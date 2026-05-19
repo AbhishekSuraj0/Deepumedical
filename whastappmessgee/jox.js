3084af4f9563acd8359ba9d1a50e910744c7e4d3
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-05-19 10:48:00 +0530

    ddddd

--- whastappmessgee/jox.js
+++ whastappmessgee/jox.js
@@ -1,3 +1,5 @@
+const scdata = "https://script.google.com/macros/s/AKfycbyc8dj5vgdZ9jH9LsCvv-2FIlqKbxwrSxOiv_pTB8Q0zUq-0diOpntaqbQaIM2Acuf4/exec"
+
 fetch("https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/whatsappdata")
     .then(res => res.json())
     .then(data => {
@@ -13,24 +15,10 @@ fetch("https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/wha
             } else {
                 div.style.backgroundColor = "rgb(238, 80, 12)";
             }
-
             div.addEventListener("click", () => {
                 window.open(`https://wa.me/${element.numberx}`)
-            })
-
-
-
-
-
-
-
+                 })
             document.querySelector(".maindiv").appendChild(div);
         })
-
-
-
-
-
-
     })
     .catch(err => { console.log(err) })
