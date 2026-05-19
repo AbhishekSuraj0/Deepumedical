f16481a1c3c78f17bd42933f81ca041104e01e39
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-05-19 12:06:55 +0530

    kjbgy

--- whastappmessgee/jox.js
+++ whastappmessgee/jox.js
@@ -1,46 +1,62 @@
 const scdata =
-"https://script.google.com/macros/s/AKfycbyc8dj5vgdZ9jH9LsCvv-2FIlqKbxwrSxOiv_pTB8Q0zUq-0diOpntaqbQaIM2Acuf4/exec";
+    "https://script.google.com/macros/s/AKfycbyc8dj5vgdZ9jH9LsCvv-2FIlqKbxwrSxOiv_pTB8Q0zUq-0diOpntaqbQaIM2Acuf4/exec";
 
 fetch("https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/whatsappdata")
-.then(res => res.json())
-.then(data => {
+    .then(res => res.json())
+    .then(data => {
 
-    document.querySelector(".maindiv").innerHTML = "";
+        document.querySelector(".maindiv").innerHTML = "";
 
-    data.forEach(element => {
+        data.forEach(element => {
 
-        const div = document.createElement("div");
-        div.classList.add("div1");
-        div.innerHTML = element.numberx;
+            const div = document.createElement("div");
+            div.classList.add("div1");
+            div.innerHTML = element.numberx;
 
-        // Color set
-        if(element.color.trim().toLowerCase() === "green"){
-            div.style.backgroundColor = "rgb(141, 236, 106)";
-        }else{
-            div.style.backgroundColor = "rgb(247, 151, 109)";
-        }
-        // Click Event
-        div.addEventListener("click", () => {
-            fetch(scdata,{
-                method:"POST",
-                mode:"no-cors",          
-                body:JSON.stringify({
-                    number: element.numberx,
-                    color: "red"
+            // Color set
+            if (element.color.trim().toLowerCase() === "green") {
+                div.style.backgroundColor = "rgb(141, 236, 106)";
+            } else {
+                div.style.backgroundColor = "rgb(247, 151, 109)";
+            }
+            // Click Event
+            div.addEventListener("click", () => {
+                fetch(scdata, {
+                    method: "POST",
+                    mode: "no-cors",
+                    body: JSON.stringify({
+                        number: element.numberx,
+                        color: "red"
+                    })
                 })
-            })
-            .then(res => res.text())
-            .then(result => {
-                div.style.backgroundColor = "rgb(246, 127, 76)";
+                    .then(res => res.text())
+                    .then(result => {
+                        div.style.backgroundColor = "rgb(246, 127, 76)";
 
-            })            
-            .catch(err => console.log(err));
-            window.open(`https://wa.me/91${element.numberx}`)
+                    })
+                    .catch(err => console.log(err));
+                window.open(`https://wa.me/91${element.numberx}`)
+            });
+
+            document.querySelector(".maindiv").appendChild(div);
+
         });
 
-        document.querySelector(".maindiv").appendChild(div);
+    })
+    .catch(err => console.log(err));
 
-    });
 
-})
-.catch(err => console.log(err));
+
+document.getElementById("inputdata").addEventListener("change", (e) => {
+    fetch('https://api.npoint.io/38bf6272169995caf595')
+        .then(res => res.json())
+        .then(data => {
+            if(e.target.value === data.password){
+                document.querySelector(".maindiv").style.display = "block";
+                e.target.style.display = "none";
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+
