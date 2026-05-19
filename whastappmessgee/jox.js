2f791e436c039c8b1cbc5343caf24ee61bf47797
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-05-19 11:09:57 +0530

    ddddd

--- whastappmessgee/jox.js
+++ whastappmessgee/jox.js
@@ -1,24 +1,52 @@
-const scdata = "https://script.google.com/macros/s/AKfycbyc8dj5vgdZ9jH9LsCvv-2FIlqKbxwrSxOiv_pTB8Q0zUq-0diOpntaqbQaIM2Acuf4/exec"
+const scdata =
+"https://script.google.com/macros/s/AKfycbyc8dj5vgdZ9jH9LsCvv-2FIlqKbxwrSxOiv_pTB8Q0zUq-0diOpntaqbQaIM2Acuf4/exec";
 
 fetch("https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/whatsappdata")
-    .then(res => res.json())
-    .then(data => {
+.then(res => res.json())
+.then(data => {
 
-        data.forEach(element => {
-            console.log(element);
-            const div = document.createElement("div");
-            div.classList.add("div1");
-            div.innerHTML = `${element.numberx}`
+    document.querySelector(".maindiv").innerHTML = "";
 
-            if(element.color === "green"){
-                div.style.backgroundColor = "rgb(68, 232, 9)";
-            } else {
+    data.forEach(element => {
+
+        const div = document.createElement("div");
+
+        div.classList.add("div1");
+
+        div.innerHTML = element.numberx;
+
+        // Color set
+        if(element.color.trim().toLowerCase() === "green"){
+
+            div.style.backgroundColor = "rgb(68, 232, 9)";
+
+        }else{
+
+            div.style.backgroundColor = "rgb(238, 80, 12)";
+        }
+
+        // Click Event
+        div.addEventListener("click", () => {
+            fetch(scdata,{
+                method:"POST",
+                mode:"no-cors",          
+                body:JSON.stringify({
+                    number: element.numberx,
+                    color: "red"
+                })
+            })
+            .then(res => res.text())
+            .then(result => {
                 div.style.backgroundColor = "rgb(238, 80, 12)";
-            }
-            div.addEventListener("click", () => {
-                window.open(`https://wa.me/${element.numberx}`)
-                 })
-            document.querySelector(".maindiv").appendChild(div);
-        })
-    })
-    .catch(err => { console.log(err) })
+
+            })            
+            .catch(err => console.log(err));
+            window.open(`https://wa.me/${element.numberx}`,"_blank")
+        });
+
+        document.querySelector(".maindiv").appendChild(div);
+
+    });
+
+})
+.catch(err => console.log(err));
